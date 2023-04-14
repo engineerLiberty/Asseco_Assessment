786003062f3beb62a7db8985a5b0095e5f0e34cd
IMOBI LIBERTY <107262741+engineerLiberty@users.noreply.github.com>
2023-04-14 12:45:39 +0100

    Final solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,33 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Sample Array");
+
+        int[] numbers = {3,3,5,5,7,7,9,6,6};
+
+        System.out.println("Numbers: " + Arrays.toString(numbers));
+
+
+        Solution1 s = new Solution1();
+
+        int unPairedElement = s.solution(numbers);
+
+        System.out.println(unPairedElement);
+
+        System.out.println("Question2 Test!");
+
+        Solution2 s2 = new Solution2();
+
+        System.out.println(s2.solution(529));
+
+
+        Solution3 s3 = new Solution3();
+
+        System.out.println(s3.getPrimes(numbers));
+
     }
 }
